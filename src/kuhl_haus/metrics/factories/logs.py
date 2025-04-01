50130a9695353e6106792a9315c37ba78374a0a6
Tom Pounders <git@oldschool.engineer>
2025-04-01 12:03:03 -0700

    Added coverage for logger factory

--- src/kuhl_haus/metrics/factories/logs.py
+++ src/kuhl_haus/metrics/factories/logs.py
@@ -1,4 +1,4 @@
-import logging
+from logging import Logger, getLogger, StreamHandler, FileHandler, Formatter
 from pathlib import Path
 from functools import lru_cache
 
@@ -6,14 +6,10 @@ from kuhl_haus.metrics.env import LOG_LEVEL
 
 
 @lru_cache()
-def get_logger(application_name: str = None, log_level: str = LOG_LEVEL, log_directory: str = None) -> logging.Logger:
+def get_logger(application_name: str = None, log_level: str = LOG_LEVEL, log_directory: str = None) -> Logger:
     application_name = __name__ if application_name is None else application_name
-    logger: logging.Logger = logging.getLogger(application_name)
+    logger: Logger = getLogger(application_name)
     logger.setLevel(log_level)
-
-    # Stream Handler (console)
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(log_level)
     logging_template = '{' \
         '"timestamp": "%(asctime)s", ' \
         '"filename": "%(filename)s", ' \
@@ -24,14 +20,18 @@ def get_logger(application_name: str = None, log_level: str = LOG_LEVEL, log_dir
         '"thr": "%(thread)d", ' \
         '"message": "%(message)s", ' \
         '}'
-    log_formatter = logging.Formatter(logging_template)
+    log_formatter = Formatter(logging_template)
+
+    # Stream Handler (console)
+    stream_handler = StreamHandler()
+    stream_handler.setLevel(log_level)
     stream_handler.setFormatter(log_formatter)
     logger.addHandler(stream_handler)
 
     # File Handler
-    if log_directory is not None:
+    if log_directory is not None or "":
         Path(log_directory).mkdir(parents=True, exist_ok=True)
-        file_handler = logging.FileHandler("{0}/{1}.log".format(log_directory, application_name))
+        file_handler = FileHandler("{0}/{1}.log".format(log_directory, application_name))
         file_handler.setLevel(log_level)
         file_handler.setFormatter(log_formatter)
         logger.addHandler(file_handler)
